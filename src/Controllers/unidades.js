9cdc0cc24cdcc0055019e42bc5201b7301b81fbd
Marco <marcus26.ma@gmail.com>
2021-03-10 10:16:26 -0600

    Unidades

---
+++ src/Controllers/unidades.js
@@ -0,0 +1,94 @@
+const datosCtrl = {};
+
+// Models
+const Unidad = require("../models/Unidades");
+
+datosCtrl.renderUnidadForm = (req, res) => {
+    res.render("Unidades/new-unidades");
+  };
+
+datosCtrl.createNewUni = async (req, res) => {
+    const { NoUnidad , Placas , NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
+        Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
+        Plazo, Contrato, FormaPag, OBD, Date } = req.body;
+        const errors = [];
+       if (!NoUnidad) 
+       {
+           errors.push({text: 'Por favor Inserta el No. de Unidad'});
+       }
+           
+       if (errors.length > 0)
+       {
+       res.render('Unidades/new-unidad', {
+       errors,NoUnidad
+       });
+       }   else {
+           const NewUnidad = new Unidad ({ NoUnidad, Placas, NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
+           Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
+           Plazo, Contrato, FormaPag, OBD, Date });
+           await NewUnidad.save();
+           res.redirect('/unidades');
+           }
+ };
+
+ datosCtrl.renderUnidad = async (req, res) => {
+    await Unidad.find()
+    .then(documentos => {
+        const datos = {
+            camioneta: documentos.map(documentos => {
+                return {
+                    _id: documentos._id,
+                    NoUnidad: documentos.NoUnidad, 
+                    Placas: documentos.Placas,
+                    NoCircula: documentos.NoCircula, 
+                    Verificacion: documentos.Verificacion, 
+                    ValorFactura: documentos.ValorFactura, 
+                    Serie: documentos.Serie, 
+                    Marca: documentos.Marca, 
+                    SubMarca: documentos.SubMarca,
+                    Modelo: documentos.Modelo, 
+                    NoMotor: documentos.NoMotor, 
+                    Poliza: documentos.Poliza, 
+                    Seguro: documentos.Seguro, 
+                    Vigencia: documentos.Vigencia, 
+                    Beneficiario: documentos.Beneficiario, 
+                    Prestamo: documentos.Prestamo, 
+                    Credito: documentos.Credito, 
+                    Mensualidad: documentos.Mensualidad,
+                    Plazo: documentos.Plazo, 
+                    Contrato: documentos.Contrato, 
+                    FormaPag: documentos.FormaPag,
+                    OBD: documentos.OBD,
+                    Date: documentos.Date
+                }
+            })
+        }
+    console.log(datos);
+    res.render('Unidades/all-unidades', { camioneta: datos.camioneta })
+    })
+};
+
+datosCtrl.renderUnidadtForm = async (req, res) => {
+    const emplo = await Unidad.findById(req.params.id).lean();
+    console.log(emplo);
+    res.render("Unidades/edit-unidad", { emplo });
+};
+
+datosCtrl.updateUnidad = async (req, res) => {
+    const { NoUnidad , Placas , NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
+        Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
+        Plazo, Contrato, FormaPag, OBD, Date  }= req.body;
+        console.log(req.body);
+        
+
+    await Unidad.findByIdAndUpdate(req.params.id, { NoUnidad , Placas , NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
+        Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
+        Plazo, Contrato, FormaPag, OBD, Date }); 
+        console.log(req.body);
+        res.redirect("/unidades");
+};
+
+datosCtrl.deleteUnidad = async (req, res) => {
+    await Unidad.findByIdAndDelete(req.params.id);
+    res.redirect("/unidades");
+};
--- src/routes/unidades.js
+++ src/routes/unidades.js
@@ -1,69 +1,36 @@
 const express = require  ('express');
 const router = express.Router();
 
-const Unidad = require('../models/Unidades');
+// Controller
+const {
+    renderUnidadForm,
+    createNewUni,
+    renderUnidad,
+    renderUnidadtForm,
+    updateUnidad,
+    deleteUnidad,
+    } = require("../Controllers/unidades");
 
-router.get('/unidades/add', (req, res)=>{
-    res.render('Unidades/new-unidad');
-});
 
-router.post('/Unidades/new-unidad', async (req,res) =>{
-const { NoUnidad , Placas , NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
- Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
- Plazo, Contrato, FormaPag, OBD, Date } = req.body;
- const errors = [];
-if (!NoUnidad) 
-{
-    errors.push({text: 'Por favor Inserta el No. de Unidad'});
-}
+// New Capacitacion
+router.get("/unidades/add", renderUnidadForm);
+
+router.post("/Unidades/new-unidad", createNewUni);
+
+// Mostrar todos los empleados
+router.get("/unidades",   renderUnidad);
     
-if (errors.length > 0)
-{
-res.render('Unidades/new-unidad', {
-errors,NoUnidad
-});
-}   else {
-    const NewUnidad = new Unidad ({ NoUnidad, Placas, NoCircula, Verificacion, ValorFactura, Serie, Marca, SubMarca,
-    Modelo, NoMotor, Poliza, Seguro, Vigencia, Beneficiario, Prestamo, Credito, Mensualidad,
-    Plazo, Contrato, FormaPag, OBD, Date });
-    await NewUnidad.save();
-    res.redirect('/unidades');
-    }
-});
-router.get('/unidades', async (req, res)=>{
-    await Unidad.find()
-    .then(documentos => {
-        const datos = {
-            camioneta: documentos.map(documentos => {
-                return {
-                    NoUnidad: documentos.NoUnidad, 
-                    Placas: documentos.Placas,
-                    NoCircula: documentos.NoCircula, 
-                    Verificacion: documentos.Verificacion, 
-                    ValorFactura: documentos.ValorFactura, 
-                    Serie: documentos.Serie, 
-                    Marca: documentos.Marca, 
-                    SubMarca: documentos.SubMarca,
-                    Modelo: documentos.Modelo, 
-                    NoMotor: documentos.NoMotor, 
-                    Poliza: documentos.Poliza, 
-                    Seguro: documentos.Seguro, 
-                    Vigencia: documentos.Vigencia, 
-                    Beneficiario: documentos.Beneficiario, 
-                    Prestamo: documentos.Prestamo, 
-                    Credito: documentos.Credito, 
-                    Mensualidad: documentos.Mensualidad,
-                    Plazo: documentos.Plazo, 
-                    Contrato: documentos.Contrato, 
-                    FormaPag: documentos.FormaPag,
-                    OBD: documentos.OBD,
-                    Date: documentos.Date
-                }
-            })
-        }
-    console.log(datos);
-    res.render('Unidades/all-unidades', { camioneta: datos.camioneta })
-    })
-});
 
-module.exports = router;
+// Delete Empleado
+router.delete("/unidades/delete/:id", deleteUnidad);
+
+//Editar un Empleado
+router.get("/unidades/edit/:id", renderUnidadtForm);
+router.put("/Unidades/edit-unidad/:id",updateUnidad);
+
+
+//Buscar una Capacitacion
+
+
+
+ module.exports = router;
